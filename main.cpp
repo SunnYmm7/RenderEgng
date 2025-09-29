547ae633a12ee406f8255226d02177ee57220132
SunnYmm7 <manoharmukadam11@gmail.com>
2025-09-29 20:55:26 +0530

    added masti

--- main.cpp
+++ main.cpp
@@ -19,7 +19,7 @@ if (window == NULL)
     std::cout << "Failed to create GLFW window" << std::endl;
     glfwTerminate();
     return -1;
-    // i love coke
+    S
     ilobeee
 }
 glfwMakeContextCurrent(window);

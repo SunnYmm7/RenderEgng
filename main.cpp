1e7f14d3c8b0acf76dc83ba6ba5f1b061549b9c7
SunnYmm7 <manoharmukadam11@gmail.com>
2025-12-30 12:38:31 +0530

    3D Cube

--- main.cpp
+++ main.cpp
@@ -3,84 +3,92 @@
 #include <GLFW/glfw3.h>
 #include <iostream>
 
+// GLM
+#include <glm/glm.hpp>
+#include <glm/gtc/matrix_transform.hpp>
+#include <glm/gtc/type_ptr.hpp>
+
 // ---------------- SHADERS ----------------
 
-// Vertex Shader
 const char* vertexShaderSource = R"(
 #version 330 core
-layout (location = 0) in vec2 aPos;
+layout (location = 0) in vec3 aPos;
+
+uniform mat4 MVP;
 
 void main()
 {
-    gl_Position = vec4(aPos, 0.0, 1.0);
+    gl_Position = MVP * vec4(aPos, 1.0);
 }
 )";
 
-// Fragment Shader
 const char* fragmentShaderSource = R"(
 #version 330 core
 out vec4 FragColor;
 
-uniform vec4 ourColor;
-
 void main()
 {
-    FragColor = ourColor;
+    FragColor = vec4(0.6, 0.1, 0.1, 1.0);
 }
 )";
 
 int main()
 {
     // ---------------- GLFW INIT ----------------
-    if (!glfwInit())
-    {
-        std::cout << "Failed to initialize GLFW\n";
-        return -1;
-    }
-
+    glfwInit();
     glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
     glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
     glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
     glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
 
-    GLFWwindow* window = glfwCreateWindow(800, 600, "Square with EBO (macOS)", nullptr, nullptr);
-    if (!window)
-    {
-        std::cout << "Failed to create GLFW window\n";
-        glfwTerminate();
-        return -1;
-    }
-
+    GLFWwindow* window = glfwCreateWindow(800, 600, "3D Cube", nullptr, nullptr);
     glfwMakeContextCurrent(window);
 
-    // ---------------- SHADER SETUP ----------------
-    unsigned int vertexShader = glCreateShader(GL_VERTEX_SHADER);
-    glShaderSource(vertexShader, 1, &vertexShaderSource, nullptr);
-    glCompileShader(vertexShader);
+    // ---------------- SHADERS ----------------
+    unsigned int vs = glCreateShader(GL_VERTEX_SHADER);
+    glShaderSource(vs, 1, &vertexShaderSource, nullptr);
+    glCompileShader(vs);
 
-    unsigned int fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
-    glShaderSource(fragmentShader, 1, &fragmentShaderSource, nullptr);
-    glCompileShader(fragmentShader);
+    unsigned int fs = glCreateShader(GL_FRAGMENT_SHADER);
+    glShaderSource(fs, 1, &fragmentShaderSource, nullptr);
+    glCompileShader(fs);
 
     unsigned int shaderProgram = glCreateProgram();
-    glAttachShader(shaderProgram, vertexShader);
-    glAttachShader(shaderProgram, fragmentShader);
+    glAttachShader(shaderProgram, vs);
+    glAttachShader(shaderProgram, fs);
     glLinkProgram(shaderProgram);
 
-    glDeleteShader(vertexShader);
-    glDeleteShader(fragmentShader);
+    glDeleteShader(vs);
+    glDeleteShader(fs);
 
-    // ---------------- SQUARE DATA ----------------
+    // ---------------- CUBE DATA ----------------
+
     float vertices[] = {
-        -0.5f,  0.5f,   // top-left
-        -0.5f, -0.5f,   // bottom-left
-         0.5f, -0.5f,   // bottom-right
-         0.5f,  0.5f    // top-right
+        // front
+        -0.5f, -0.5f,  0.5f,
+         0.5f, -0.5f,  0.5f,
+         0.5f,  0.5f,  0.5f,
+        -0.5f,  0.5f,  0.5f,
+        // back
+        -0.5f, -0.5f, -0.5f,
+         0.5f, -0.5f, -0.5f,
+         0.5f,  0.5f, -0.5f,
+        -0.5f,  0.5f, -0.5f
     };
 
     unsigned int indices[] = {
-        0, 1, 2,
-        0, 2, 3
+        // front
+        0, 1, 2, 0, 2, 3,
+        // back
+        4, 5, 6, 4, 6, 7,
+        // left
+        4, 0, 3, 4, 3, 7,
+        // right
+        1, 5, 6, 1, 6, 2,
+        // top
+        3, 2, 6, 3, 6, 7,
+        // bottom
+        4, 5, 1, 4, 1, 0
     };
 
     unsigned int VAO, VBO, EBO;
@@ -90,33 +98,54 @@ int main()
 
     glBindVertexArray(VAO);
 
-    // VBO
     glBindBuffer(GL_ARRAY_BUFFER, VBO);
     glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);
 
-    // EBO
     glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, EBO);
     glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
 
-    // Vertex attributes
-    glVertexAttribPointer(0, 2, GL_FLOAT, GL_FALSE, 2 * sizeof(float), (void*)0);
+    glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 3 * sizeof(float), (void*)0);
     glEnableVertexAttribArray(0);
 
     glBindVertexArray(0);
 
+    // ---------------- 3D SETTINGS ----------------
+    glEnable(GL_DEPTH_TEST);
+
     // ---------------- RENDER LOOP ----------------
     while (!glfwWindowShouldClose(window))
     {
-        glClearColor(0.1f, 0.2f, 0.2f, 1.0f);
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClearColor(0.1f, 0.1f, 0.15f, 1.0f);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         glUseProgram(shaderProgram);
         glBindVertexArray(VAO);
 
-        int colorLoc = glGetUniformLocation(shaderProgram, "ourColor");
-        glUniform4f(colorLoc, 0.0f, 1.0f, 0.0f, 1.0f); // green square
+        // MATRICES
+        glm::mat4 model = glm::rotate(
+            glm::mat4(1.0f),
+            (float)glfwGetTime(),
+            glm::vec3(0.5f, 1.0f, 0.0f)
+        );
 
-        glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+        glm::mat4 view = glm::translate(
+            glm::mat4(1.0f),
+            glm::vec3(0.0f, 0.0f, -3.0f)
+        );
+
+        glm::mat4 projection = glm::perspective(
+            glm::radians(45.0f),
+            800.0f / 600.0f,
+            0.1f,
+            100.0f
+        );
+
+        glm::mat4 MVP = projection * view * model;
+
+        int mvpLoc = glGetUniformLocation(shaderProgram, "MVP");
+        glUniformMatrix4fv(mvpLoc, 1, GL_FALSE, glm::value_ptr(MVP));
+
+        glDrawElements(GL_TRIANGLES, 36, GL_UNSIGNED_INT, 0);
 
         glfwSwapBuffers(window);
         glfwPollEvents();
